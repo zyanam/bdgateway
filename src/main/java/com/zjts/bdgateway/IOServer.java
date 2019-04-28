565c78238c324ef74b5958ec65b87b6f5705e04a
yanam <98346@sina.com>
2019-04-28 20:57:59 +0800

    socket

--- src/main/java/com/zjts/bdgateway/IOServer.java
+++ src/main/java/com/zjts/bdgateway/IOServer.java
@@ -1,6 +1,7 @@
 package com.zjts.bdgateway;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,9 +9,35 @@ public class IOServer {
     public static void main(String[] args) throws Exception {
         ServerSocket serverSocket = new ServerSocket(8899);
 
-        new Thread(()-> {
-            while (true) {
-                Socket socket = serverSocket.accept();
+        new Thread(() -> {
+
+            try {
+                while (true) {
+                    Socket socket = serverSocket.accept();
+                    new Thread(() -> {
+                        try {
+                            byte[] buffer = new byte[1024];
+                            InputStream inputStream = socket.getInputStream();
+                            while (true) {
+                                int len;
+                                while ((len = inputStream.read(buffer)) != -1) {
+                                    for (int i = 0; i < len; i++) {
+                                        byte b = buffer[i];
+                                        int it = b & 0xff;
+                                        System.out.println(Integer.toHexString(it));
+                                    }
+                                }
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } finally {
+                        }
+                    }
+                    ).start();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
             }
         }).start();
     }
